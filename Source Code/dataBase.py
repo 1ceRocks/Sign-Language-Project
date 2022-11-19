086d0d71036330788d577111a4c46444da3490cd
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-19 11:15:51 +0800

    Basic Hand Detection

--- Source Code/dataBase.py
+++ Source Code/dataBase.py
@@ -1,7 +1,17 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
+
 cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands = 1)
 
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hands = hands[0]
+        x, y, w, h = hands['bbox']
+        imgCrop = img[y:y + h, x:x + w]
+        cv2.imshow("ImageCrop", imgCrop)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
