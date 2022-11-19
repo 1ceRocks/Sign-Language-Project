4c114695b2712a88381e378818f2879870347632
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-19 11:49:26 +0800

    Error in Hands and Image Size

--- Source Code/dataBase.py
+++ Source Code/dataBase.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands = 1)
@@ -19,7 +20,14 @@ while True:
 
         imgCropShape = imgCrop.shape
 
-        imgWhite[0:imgCropShape[0], 0:imgCropShape[1]] = imgCrop
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgCrop.shape
+            imgWhite[0:imgResizeShape[0], 0:imgResizeShape[1]] = imgCrop
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
