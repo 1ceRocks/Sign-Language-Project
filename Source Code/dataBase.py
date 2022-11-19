e95d5499cae8c7a3c0514e3e689a6e7fc3a8c75e
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-19 14:22:11 +0800

    Code are now inputting A B C and is now designed with special colors such as pink.

--- Source Code/dataBase.py
+++ Source Code/dataBase.py
@@ -17,13 +17,13 @@ labels = ["A", "B", "C"]
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
-    hands = detector.findHands(img, draw = False)
+    hands, img = detector.findHands(img)
 
     if hands:
         hands = hands[0]
         x, y, w, h = hands['bbox']
 
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255    
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255    
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
         imgCropShape = imgCrop.shape
@@ -36,7 +36,7 @@ while True:
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap: wCal + wGap] = imgResize
+            imgWhite[:, wGap:wCal + wGap] = imgResize
             prediction, index = classifier.getPrediction(imgWhite, draw = False)
             print(prediction, index)
 
@@ -46,14 +46,12 @@ while True:
             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap : hCal + hGap, :] = imgResize
+            imgWhite[hGap:hCal + hGap, :] = imgResize
             prediction, index = classifier.getPrediction(imgWhite, draw = False)
 
-        cv2.rectangle(imgOutput, (x - offset, y - offset-50), 
-                        (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
-        cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 0, 255), 2)
-        cv2.rectangle(imgOutput, (x-offset, y-offset),
-                        (x + w+offset, y + h+offset), (255, 0, 255), 4)
+        cv2.rectangle(imgOutput, (x - offset, y - offset-50), (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset), (x + w+offset, y + h+offset), (255, 0, 255), 4)
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
