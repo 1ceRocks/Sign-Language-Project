d5ce391f701f9726254849a03675aff367d7c401
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-19 10:41:49 +0800

    General Description as Comment

---
+++ Source Code/hand-detection.py
@@ -0,0 +1,4 @@
+# Python Project (DSA - Seatwork 3)
+# Concept: Easy Sign Language Detection (Alphabets)
+# Credit for Murtaza's Workshop - Robotics and AI Channel
+# https://www.youtube.com/watch?v=wa2ARoUUdU8
